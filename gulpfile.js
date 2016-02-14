42d7ec8fa68f2eecb0c0ffbe6c1af559abdb971c
Maciej Kurek <maciejk77@gmail.com>
2016-02-14 18:15:18 +0000

    Second Gulp task uglify applied

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp'),
-  minifyCSS = require('gulp-minify-css');
+  minifyCSS = require('gulp-minify-css'),
+  uglify = require('gulp-uglify');
 
   gulp.task('style', function() {
     return gulp
       .src('css/style.css')
       .pipe(minifyCSS())
       .pipe(gulp.dest('assets'));
-  })
+  });
+
+  gulp.task('script', function() {
+    return gulp
+      .src('js/script.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('assets'));
+  });
